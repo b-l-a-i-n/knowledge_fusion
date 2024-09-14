d6de1251b7771ede6db86ab3b3af82a42ce8b85d
natriistorm <natriistorm@mail.ru>
2024-09-14 12:33:55 +0300

    Update script

--- src/ue_metrics/calc_metrics.py
+++ src/ue_metrics/calc_metrics.py
@@ -4,7 +4,7 @@ from transformers.utils import logging
 from transformers import AutoModelForCausalLM
 logging.set_verbosity_error() 
 import os
-os.environ['CUDA_VISIBLE_DEVICES']="2,3"
+os.environ['CUDA_VISIBLE_DEVICES']="3"
 import torch
 from tqdm import tqdm
 from lm_polygraph import WhiteboxModel
@@ -17,7 +17,8 @@ from transformers import AutoTokenizer
 from sklearn.utils import shuffle
 from datasets import load_dataset
 
-model_name = 'mistralai/Mistral-7B-Instruct-v0.1'
+#model_name = 'mistralai/Mistral-7B-Instruct-v0.1'
+model_name = 'meta-llama/Meta-Llama-3.1-8B'
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = WhiteboxModel.from_pretrained(
     model_name, device_map='auto',
@@ -27,94 +28,189 @@ from criteria import StopWordCriteria
 stop_words = ["\n", "QUESTION"]
 stopping_criteria = StopWordCriteria(tokenizer=tokenizer, prompts=[], stop_words=stop_words)
 from greedy_probs import GreedyProbsCalculator
-from prompts import combine_two_contexts. make_no_context_prompt, make_prompt
+from prompts import combine_two_contexts, make_no_context_prompt, make_prompt
 
-def convert_epsilon_to_text(d: dict):
-    entity = f'{d['entity']['label']} is a {d['entity']['description']}\n'
-    text = entity
-    for n in d['neighbors']:
-        label = f'{n['neighbor_label']} is a {n['neighbor_description']}\n'
-        text += label
-    return text
+name = 'medicine'
+ddg_path = f'../fusion/knowledge_fusion/data/retrieve_to_models/{name}_mmlu/duckduckgo.csv'
+google_path = f'../fusion/knowledge_fusion/data/retrieve_to_models/{name}_mmlu/google.csv'
+wikipedia_path = f'../fusion/knowledge_fusion/data/retrieve_to_models/{name}_mmlu/wikipedia.csv'
+general_path = f'../fusion/knowledge_fusion/data/retrieve_to_models/{name}_mmlu.csv'
+
+
+retrieved_truthful_qa_mc_1 = pd.read_csv(ddg_path)
+retrieved_truthful_qa_mc_2 = pd.read_csv(google_path)
+retrieved_truthful_qa_mc_3 = pd.read_csv(wikipedia_path)
+ds = pd.read_csv(general_path, index_col=[0])
+
+
+def predict(
+    ds, 
+    size_wikidata=None, 
+    size_google=500,
+    size_ddg=500,
+    size_wikipedia=500,
+    number_of_context=5, 
+    **kwargs
+):
+    ddg_df = pd.read_csv(ddg_path) if "ddg_path" in kwargs else None
+    google_df = pd.read_csv(google_path) if "google_path" in kwargs else None
+    wikidata_df = pd.read_csv(wikidata_path) if "wikidata_path" in kwargs else None
+    wikipedia_df = pd.read_csv(wikipedia_path) if "wikipedia_path" in kwargs else None
+    
+    stats, correct_labels, ue_metrics = [], [], []
+    for idx, row in tqdm(retrieved_truthful_qa_mc_1.iterrows(), total=retrieved_truthful_qa_mc_1.shape[0]):
+        question = row['question']
+        # answers, labels = shuffle(ds[idx]['mc1_targets']['choices'], ds[idx]['mc1_targets']['labels'], random_state=0)
+        answers = eval(ds.iloc[idx].choices)
+        correct_labels.append(ds.iloc[idx].answer)
         
-
-ds = load_dataset("truthfulqa/truthful_qa", "multiple_choice")['validation']
-retrieved_ddg_path = '../../data/retrieve_to_models/truthfulqa_multichoice/duckduckgo.csv'
-retrieved_google_path = '../../data/retrieve_to_models/truthfulqa_multichoice/google.csv'
-retrieved_wikipedia_path = '../../data/retrieve_to_models/truthfulqa_multichoice/wikipedia.csv'
-retrieved_wikidata_path = '../../data/retrieve_to_models/truthfulqa_multichoice/wikidata.csv'
-retrieved_truthful_qa_mc = pd.read_csv(retrieved_google_path)
-retrieved_truthful_qa_mc_add = = pd.read_csv(retrieved_ddg_path)
-
-
-stats = []
-ue_metrics = []
-correct_labels = []
-for idx, row in tqdm(retrieved_truthful_qa_mc.iterrows()):
-    question = row['question']
-    contexts = []
-    answers, labels = shuffle(ds[idx]['mc1_targets']['choices'], ds[idx]['mc1_targets']['labels'],random_state=0)
-    correct_labels.append(labels)
-    number_of_context = len(row.index.values) - 2
-    
-    for i in range (1, number_of_context + 1):
-        # wikidata
-        # if type(row[f'context_{i}']) is not str:
-            # break
-        # text = convert_epsilon_to_text(eval(row[f'context_{i}']))
-        # contexts.append(text)
+        contexts = {key: [] for key in ["wikipedia", "wikidata", "ddg", "google"]}
+        for i in range (1, number_of_context + 1):
+            
+            if wikidata_df is not None and type(wikidata_df[f'context_{i}'].iloc[idx]) is str:
+                if type(wikidata_df[f'context_{i}'].iloc[idx]) is str:
+                    text = convert_epsilon_to_text(eval(wikidata_df[f'context_{i}'].iloc[idx]))
+                    contexts["wikidata"].append(text[:size_wikidata])
+            
+            if wikipedia_df is not None and type(wikipedia_df[f'context_{i}'].iloc[idx]) is str:
+                ctx = eval(wikipedia_df[f'context_{i}'].iloc[idx])
+                if len(ctx) == 0:
+                    continue
+                elif ctx['is_summary'] is True:
+                    contexts["wikipedia"].append(ctx['context'][:size_wikipedia])
+                else:
+                    contexts["wikipedia"].append(ctx['context'][:size_wikipedia])
+            
+            if ddg_df is not None and type(ddg_df[f'context_{i}'].iloc[idx]) is str:
+                contexts["ddg"].append(ddg_df[f'context_{i}'].iloc[idx][:size_ddg])
+            
+            if google_df is not None and type(google_df[f'context_{i}'].iloc[idx]) is str:
+                contexts["google"].append(google_df[f'context_{i}'].iloc[idx][:size_google])
+                        
+        if len([c for c in contexts.values() if c]):
+            texts = [
+                combine_two_contexts(
+                    [
+                        "\n".join(["- " + s.replace("\n", " ") + "..." for s in c]) 
+                        for c in contexts.values() if c
+                    ], 
+                    question,
+                    answers
+                )
+            ]
+        else:
+            texts = [
+                make_no_context_prompt(
+                    question,
+                    answers
+                )
+            ]
         
-        # wikipedia
-        # ctx = eval(row[f'context_{i}'])
-        # if len(ctx) == 0:
-        #     continue
-        # elif ctx['is_summary'] is True:
-        #     contexts.append(ctx['context'])
-        # else:
-        #     contexts.append(ctx['context'][:500])
+        stat = {}
+        flg = False
+        for calculator in [
+            GreedyProbsCalculator()
+        ]:
+            try:
+                stat.update(calculator(stat, texts, model))    
+            except IndexError as err:
+                print(texts)
+                flg = True
+                break
+        if flg:
+            stats.append({})
+            ue_metrics.append({})
+            continue
+        log_likelihoods = stat['greedy_log_likelihoods']
+        msp = np.array([-np.sum(log_likelihood) for log_likelihood in log_likelihoods])
+        perplexity = np.array([-np.mean(ll) for ll in log_likelihoods])
+        mean_token_entropy = np.array([np.mean(e) for e in stat['entropy']])
+        stats.append(stat)
+        ue_metrics.append({'msp': msp, 'perplexity': perplexity, 'entropy': mean_token_entropy})
+        
+    return stats, correct_labels, ue_metrics
 
-        # ddg and google
-        # contexts.append(row[f'context_{i}'])
+def save(stats, correct_labels, ue_metrics, name, mmlu_topic):
+    df = []
+    for idx, s in enumerate(stats):
+        # print(chr(ord('A') + correct_labels[idx].index(1)))
+        answers = ''
+        for v in s['greedy_texts']:
+            answers += v.strip()[0]
+        #print(answers)
+        df.append({'idx': idx, 'correct': chr(ord('A') + correct_labels[idx]), 'answers': answers,
+                   'msp': ue_metrics[idx]['msp'][0], 'perplexity': ue_metrics[idx]['perplexity'][0], 'entropy': ue_metrics[idx]['entropy'][0]})
+    d = pd.DataFrame(df)
+    d.to_csv(f'../fusion/knowledge_fusion/data/model_answers/{mmlu_topic}_mmlu/{mmlu_topic}_mmlu_{name}_llama.csv')
 
-        # several contexts
-        if type(row[f'context_{i}']) is not str:
-            break
-        contexts.append(row[f'context_{i}'])
-        google_index = random.randint(1, number_of_context_google)
-        while type(retrieved_truthful_qa_mc_add.iloc[idx][f'context_{google_index}']) is not str:
-            google_index = random.randint(1, number_of_context_google)
-        contexts.append(retrieved_truthful_qa_mc_add.iloc[idx][f'context_{google_index}'])
-    
-    # for separate contexts
-    # texts = [make_prompt(c, question, answers) for c in contexts]
 
-    # for united contexts
-    texts = [combine_two_contexts(contexts, question, answers)]
-    
-    # for empty context
-    # texts.append(make_no_context_prompt(question, answers))
-    stat = {}
-    for calculator in [
-        GreedyProbsCalculator()
-    ]:
-        stat.update(calculator(stat, texts, model))    
-    log_likelihoods = stat['greedy_log_likelihoods']
-    msp = np.array([-np.sum(log_likelihood) for log_likelihood in log_likelihoods])
-    perplexity = np.array([-np.mean(ll) for ll in log_likelihoods])
-    mean_token_entropy = np.array([np.mean(e) for e in stat['entropy']])
-    stats.append(stat)
-    ue_metrics.append({'msp': msp, 'perplexity': perplexity, 'entropy': mean_token_entropy})
+stats, correct_labels, ue_metrics = stats, correct_labels, ue_metrics = predict(
+        ds,
+        ddg_path=ddg_path,
+        # google_path=google_path,
+        # wikipedia_path=wikipedia_path,
+        # wikidata_path=wikidata_path
+        mmlu_topic='medicine'
+        )
+save(stats, correct_labels, ue_metrics, 'ddg', mmlu_topic='medicine')
 
-df_src = []
-for idx, s in enumerate(stats):
-    answers = ''
-    for v in s['greedy_texts']:
-        # mistral
-        answers += v[0]
-        # llama
-        answer += v.strip()
-    df_src.append({'idx': idx, 'correct': chr(ord('A') + correct_labels[idx].index(1)), 'answers': answers,
-               'msp': ue_metrics[idx]['msp'], 'perplexity': ue_metrics[idx]['perplexity'], 'entropy': ue_metrics[idx]['entropy']})
+stats, correct_labels, ue_metrics = stats, correct_labels, ue_metrics = predict(
+        ds,
+        # ddg_path=ddg_path,
+        google_path=google_path,
+        # wikipedia_path=wikipedia_path,
+        # wikidata_path=wikidata_path
+        mmlu_topic='medicine'
+)
+save(stats, correct_labels, ue_metrics, 'google', mmlu_topic='medicine')
 
-df = pd.DataFrame(df_src)
-df.to_csv('../../data/model_answers/truthful_qa_mc/truthful_qa_multichoice_google_mistral.csv')
+stats, correct_labels, ue_metrics = stats, correct_labels, ue_metrics = predict(
+        ds,
+        # ddg_path=ddg_path,
+        # google_path=google_path,
+        wikipedia_path=wikipedia_path,
+        # wikidata_path=wikidata_path
+        mmlu_topic='medicine'
+)
+save(stats, correct_labels, ue_metrics, 'wikipedia', mmlu_topic='medicine')
+
+stats, correct_labels, ue_metrics = stats, correct_labels, ue_metrics = predict(
+        ds,
+        ddg_path=ddg_path,
+        # google_path=google_path,
+        wikipedia_path=wikipedia_path,
+        # wikidata_path=wikidata_path
+        mmlu_topic='medicine'
+)
+save(stats, correct_labels, ue_metrics, 'ddg_wikipedia', mmlu_topic='medicine')
+
+stats, correct_labels, ue_metrics = stats, correct_labels, ue_metrics = predict(
+        ds,
+        ddg_path=ddg_path,
+        google_path=google_path,
+        # wikipedia_path=wikipedia_path,
+        # wikidata_path=wikidata_path
+        mmlu_topic='medicine'
+)
+save(stats, correct_labels, ue_metrics, 'ddg_google', mmlu_topic='medicine')
+
+stats, correct_labels, ue_metrics = stats, correct_labels, ue_metrics = predict(
+        ds,
+        # ddg_path=ddg_path,
+        google_path=google_path,
+        wikipedia_path=wikipedia_path,
+        # wikidata_path=wikidata_path
+        mmlu_topic='medicine'
+)
+save(stats, correct_labels, ue_metrics, 'google_wikipedia',mmlu_topic='medicine')
+
+stats, correct_labels, ue_metrics = stats, correct_labels, ue_metrics = predict(
+        ds,
+        ddg_path=ddg_path,
+        google_path=google_path,
+        wikipedia_path=wikipedia_path,
+        # wikidata_path=wikidata_path
+        mmlu_topic='medicine'
+)
+save(stats, correct_labels, ue_metrics, 'ddg_google_wikipedia', mmlu_topic='medicine')
+
